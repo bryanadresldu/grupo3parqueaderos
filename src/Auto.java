1d48c1c11a9e53bf2db81d5f7cd712e44e5d569b
POO5 <santiago.vargas@epn.edu.ec>
2025-05-29 09:29:15 -0500

    encapsulamiento

--- src/Auto.java
+++ src/Auto.java
@@ -1,5 +1,10 @@
-public class Auto extends Vehiculo{
-    public Auto(String placa, String modelo) {
-        super(placa, modelo);
+class Auto extends Vehiculo {
+    public Auto(String placa) {
+        super(placa);
+    }
+
+    @Override
+    public double tarifaDiaria() {
+        return 20.0;
     }
 }
